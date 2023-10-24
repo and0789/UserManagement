208a73bff29b5f25f40912d87aa9ee074aea7f1a
Andre Septian <andreseptian07@icloud.com>
2023-10-24 14:26:18 +0700

    Fixed SmsUtils

--- src/main/java/com/andreseptian/usermanagement/utils/SmsUtils.java
+++ src/main/java/com/andreseptian/usermanagement/utils/SmsUtils.java
@@ -8,9 +8,9 @@ import static com.twilio.rest.api.v2010.account.Message.creator;
 
 
 public class SmsUtils {
-    public static final String FROM_NUMBER = "6285295557633";
-    public static final String SID_KEY = "AC9014759e5d414efb6a08c1d51fe799d9";
-    public static final String TOKEN_KEY = "6f8b96f671554d223f99c73f9521d74e";
+    public static final String FROM_NUMBER = "your phone";
+    public static final String SID_KEY = "your key";
+    public static final String TOKEN_KEY = "your key";
 
     public static void sendSMS(String to, String messageBody) {
         Twilio.init(SID_KEY, TOKEN_KEY);
